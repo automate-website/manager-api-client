6398eb12a71d122cf8bf20220b9e6e941f1ba000
Michael Lewkowski <michael@lewkowski.de>
2016-08-28 21:25:13 +0200

    added fragment as property to the scenario model

--- src/main/java/website/automate/manager/api/client/model/Scenario.java
+++ src/main/java/website/automate/manager/api/client/model/Scenario.java
@@ -4,11 +4,21 @@ public class Scenario extends AbstractEntity {
 
     private String name;
     
+    private boolean fragment;
+    
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean isFragment() {
+        return fragment;
+    }
+
+    public void setFragment(boolean fragment) {
+        this.fragment = fragment;
     }
 }
